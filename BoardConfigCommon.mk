912335347f8ebf5e5baaa0117bfebe5356e568fb
andixlm <andixev@gmail.com>
2014-10-18 23:15:08 +0600

    Initial commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,94 @@
+USE_CAMERA_STUB := true
+
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a9
+TARGET_CPU_SMP := true
+
+TARGET_BOARD_PLATFORM := capri
+TARGET_BOOTLOADER_BOARD_NAME := capri
+
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000
+BOARD_KERNEL_BASE := 0xa2000000
+BOARD_KERNEL_PAGESIZE := 4096
+
+# Filesystem
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 4404019200
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/bcm281x5-common/bluetooth/libbt_vndcfg.txt
+
+# Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER 		:= NL80211
+WPA_SUPPLICANT_VERSION      		:= VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB 	:= lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        		:= NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   		:= lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           		:= bcmdhd
+BOARD_WLAN_DEVICE_REV       		:= bcm4330_b1
+WIFI_DRIVER_FW_PATH_PARAM   		:= "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     		:= "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP      		:= "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_MODULE_PATH     		:= "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_MODULE_NAME     		:= "dhd"
+WIFI_DRIVER_MODULE_ARG      		:= "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG   		:= "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_BAND                   		:= 802_11_ABG
+
+# Wi-Fi Tethering
+BOARD_HAVE_SAMSUNG_WIFI := true
+
+# SkTextBox for libtvout
+BOARD_USES_SKTEXTBOX := true
+
+# Hardware rendering
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/samsung/bcm281x5-common/configs/egl.cfg
+BOARD_USE_MHEAP_SCREENSHOT := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DCAPRI_HWC
+
+# Boot animation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/samsung/bcm281x5-common/ril/
+
+# Hardware tunables
+BOARD_HARDWARE_CLASS := device/samsung/bcm281x5-common/cmhw/
+
+# GPS
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/bcm281x5-common/include
+
+# healthd
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.capri
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/bcm281x5-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts
---
+++ common.mk
@@ -0,0 +1,136 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+COMMON_PATH := device/samsung/bcm281x5-common
+
+# Init files
+PRODUCT_COPY_FILES += \
+	$(COMMON_PATH)/ramdisk/init.log.rc:root/init.log.rc \
+	$(COMMON_PATH)/ramdisk/lpm.rc:root/lpm.rc
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
+
+# Prebuilt kl keymaps
+PRODUCT_COPY_FILES += \
+	$(COMMON_PATH)/keylayouts/bcm_headset.kl:system/usr/keylayout/bcm_headset.kl \
+	$(COMMON_PATH)/keylayouts/bcm_keypad_v2.kl:system/usr/keylayout/bcm_keypad_v2.kl \
+	$(COMMON_PATH)/keylayouts/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+	$(COMMON_PATH)/keylayouts/samsung-keypad.kl:system/usr/keylayout/samsung-keypad.kl \
+	$(COMMON_PATH)/keylayouts/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
+	$(COMMON_PATH)/keylayouts/sii9234_rcp.kl:system/usr/keylayout/sii9234_rcp.kl
+
+PRODUCT_COPY_FILES += \
+	$(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	setup_fs
+
+# USB accessory
+PRODUCT_PACKAGES += \
+	com.android.future.usb.accessory
+
+# Device-specific packages
+PRODUCT_PACKAGES += \
+	SamsungServiceMode \
+	Torch
+
+# Charger
+PRODUCT_PACKAGES += \
+	charger \
+	charger_res_images
+
+# Device Settings
+PRODUCT_PACKAGES += \
+	BCMSettings
+
+# Misc other modules
+PRODUCT_PACKAGES += \
+	audio.a2dp.default \
+	audio.usb.default \
+	audio.r_submix.default \
+	audio_policy.capri
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+# Logging
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.brcm.log=none \
+	persist.brcm.cp_crash=none \
+	persist.brcm.ap_crash=none
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+	brcm.hwc.no-hdmi-trans=1 \
+	ro.opengles.version=131072 \
+	ro.zygote.disable_gl_preload=1 \
+	debug.hwui.render_dirty_regions=false
+
+# TV out
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.tvout.enable=true
+
+# Support for Browser's saved page feature. This allows
+# for pages saved on previous versions of the OS to be
+# viewed on the current OS.
+PRODUCT_PACKAGES += \
+	libskia_legacy
+
+# These are the hardware-specific settings that are stored in system properties.
+# Note that the only such settings should be the ones that are too low-level to
+# be reachable from resources or other mechanisms.
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.interface=wlan0 \
+	mobiledata.interfaces=rmnet0 \
+	ro.telephony.ril_class=SamsungBCMRIL
+
+$(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.ril.hsxpa=1 \
+	ro.ril.gprsclass=10
+
+# Extended JNI checks
+# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
+# before they have a chance to cause problems.
+# Default=true for development builds, set by android buildsystem.
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.kernel.android.checkjni=0 \
+	dalvik.vm.checkjni=false
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
+# Dalvik heap config
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+
+# We have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+$(call inherit-product-if-exists, vendor/samsung/bcm281x5-common/bcm281x5-common-vendor.mk)
