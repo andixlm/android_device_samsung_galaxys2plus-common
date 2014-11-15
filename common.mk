ac07fd2bfd42364e81fe11e5273d5ee2ff96a217
andixlm <andixev@gmail.com>
2014-11-15 22:08:56 +0500

    Change the path of gps.cer

--- common.mk
+++ common.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	$(COMMON_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf \
 	$(COMMON_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-	$(COMMON_PATH)/configs/gps.cer:system/etc/gps.cer \
+	$(COMMON_PATH)/configs/gps.cer:system/bin/gps.cer \
 	$(COMMON_PATH)/configs/gps.conf:system/etc/gps.conf \
 	$(COMMON_PATH)/configs/gps.xml:system/etc/gps.xml \
 	$(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
