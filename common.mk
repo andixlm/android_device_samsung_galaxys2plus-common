313fa19df286b6507220bf738800f3ea982d17e8
ghsr <ghsr92@yandex.ru>
2015-09-10 12:16:17 +0600

    galaxys2plus: Switch to lower-end codecs

--- common.mk
+++ common.mk
@@ -52,7 +52,7 @@ PRODUCT_COPY_FILES += \
 	$(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
